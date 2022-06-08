684601595aa459c50a376a479050f64cd94ee754
karen chehade <karen chehade>
2022-06-08 10:18:36 +0300

    icon with transparent background added

--- lib/home-page.dart
+++ lib/home-page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -12,128 +13,144 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     List categories = ['Chinese', 'Indian', 'Italian', 'American'];
-    Color color = Colors.white;
-    bool pressAttention =true;
+    bool pressAttention = true;
+    // pressMe() {
+    //   setState(() {
+    //     pressAttention = false;
+    //   });
+    // }
+    int clickedIndex = -1;
     return Scaffold(
       appBar: AppBar(
         title: const Text('chattak'),
         backgroundColor: Colors.white,
       ),
       body: Container(
+          color: Colors.white,
           child: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.all(15),
-            width: width,
-            height: 150,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(60),
-              child: const Image(
-                image: AssetImage('images/img1.jpg'),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Container(
-              child: Column(
             children: [
-              const Text(
-                'Categories',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+              Container(
+                margin: const EdgeInsets.all(15),
+                width: width,
+                height: 100,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(60),
+                  child: const Image(
+                    image: AssetImage('assets/images/delivery.jpg'),
+                  ),
                 ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 140,
-                color: color,
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: categories.length,
-                    itemBuilder: (_, i) {
-                      return GestureDetector(
-                        onTap: () =>  setState(() => pressAttention = !pressAttention),
-                        child: Container(
-                          width: 120,
-                          height: 120,
-                          margin: const EdgeInsets.only(left: 10, right: 10),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(35),
-                            image: const DecorationImage(
-                                image: AssetImage('images/icon1.png') ),
-                                // backgroundBlendMode: BlendMode.saturation ,
-                                color: Colors.black.withOpacity(0.5)
-                          //  color: pressAttention ? Colors.grey : Colors.blue,
-                          ),
-                          child: Text(categories[i]),
-                        ),
-                      );
-                    }),
-              ),
-            ],
-          )),
-          Container(
-            child: Column(children: [
-              Container(
-                  child: const Text(
-                'Popular Item',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              )),
               const SizedBox(
                 height: 15,
               ),
+              const Text(
+                    'Categories',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
               Container(
-                height: 200,
-                child: ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  itemCount: categories.length,
-                  itemBuilder: (_, i) {
-                    return Row(
-                      children: [
-                        Container(
-                          width: 100,
-                          height: 100,
-                          margin: const EdgeInsets.all(10),
-                          child: const CircleAvatar(
-                              radius: 40,
-                              backgroundImage: AssetImage('images/img3.jpg')),
-                        ),
-                        // const SizedBox(width: 10,),
-                        Column(
-                          children: const [
-                            Text('Greek pizza'),
-                            Text('\$15.00'),
-                          ],
-                        ),
-                        const SizedBox(
-                          width: 150,
-                        ),
-                        Container(
-                          child: const Center(
-                            child: Icon(
-                              Icons.save_alt_sharp,
-                              color: Color(0xFF69c5df),
-                              size: 30,
+                  child: Column(
+                children: [
+                  
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    height: 150,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: categories.length,
+                        itemBuilder: (_, i) {
+                          return GestureDetector(
+                            // onTap: () => setState(() => clickedIndex = i),
+                            child: Container(
+                                margin: const EdgeInsets.only(
+                                    left: 10, right: 10, top: 10),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: clickedIndex == i
+                                      ? Colors.red
+                                      : Colors.white,
+                                ),
+                                child: Column(
+                                  children: [
+                                    IconButton(
+                                      iconSize: 100,
+                                      icon: SvgPicture.asset(
+                                        "assets/icons/pizza11.svg",
+                                      ),
+                                      onPressed: () {
+                                        
+                                      },
+                                    ),
+                                    Text(categories[i]),
+                                  ],
+                                )),
+                          );
+                        }),
+                  ),
+                ],
+              )),
+              Container(
+                child: Column(children: [
+                  Container(
+                      child: const Text(
+                    'Popular Item',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  )),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    height: 200,
+                    child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      itemCount: categories.length,
+                      itemBuilder: (_, i) {
+                        return Row(
+                          children: [
+                            Container(
+                              width: 100,
+                              height: 100,
+                              margin: const EdgeInsets.all(10),
+                              child: const CircleAvatar(
+                                  radius: 40,
+                                  backgroundImage:
+                                      AssetImage('assets/images/img3.jpg')),
                             ),
-                          ),
-                        )
-                      ],
-                    );
-                  },
-                ),
-              )
-            ]),
-          ),
-        ],
-      )),
+                            // const SizedBox(width: 10,),
+                            Column(
+                              children: const [
+                                Text('Greek pizza'),
+                                Text('\$15.00'),
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 150,
+                            ),
+                            Container(
+                              child: const Center(
+                                child: Icon(
+                                  Icons.save_alt_sharp,
+                                  color: Color(0xFF69c5df),
+                                  size: 30,
+                                ),
+                              ),
+                            )
+                          ],
+                        );
+                      },
+                    ),
+                  )
+                ]),
+              ),
+            ],
+          )),
     );
   }
 }
